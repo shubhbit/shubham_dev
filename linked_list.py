2a889d49ea272ee0bc76b23b6778aeff41c3dc7a
shpandey <shubham.pandey@gemalto-inc.com>
2017-06-19 13:24:28 -0400

    implementing linked list

---
+++ linked_list.py
@@ -0,0 +1,13 @@
+"""
+Document
+"""
+class Node(object):
+        def __init__(self, data):
+                self.data = data
+                self.left = None
+                self.right = None
+
+class Head(object):
+        def __init__(self):
+                self.head = None
+
